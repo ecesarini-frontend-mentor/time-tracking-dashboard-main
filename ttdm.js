0c5f448cce93a3008e44e412c386cfc82d67a56c
ecesarini <73652902+ecesarini@users.noreply.github.com>
2021-12-26 01:31:29 +0100

    published

--- ttdm.js
+++ ttdm.js
@@ -1,27 +1,6 @@
-function ttdmGetButtonName() {
-    var buttons = document.getElementsByTagName("button");
-    for (var i = 0; i <= buttons.length; i += 1) {
-        buttons[i].onclick = function() {
-           return this.getElementsByName[0];
-        }; 
+function ttdmTfClicked(myId) {
+	var rtn = document.getElementById(myId).value;
+	alert(rtn + " has been clicked")
 }
-
-function ttdmShowTimeframes(ttdmGetButtonNameCallback) {
-    var buttonName = ttdmGetButtonNameCallback();
-    switch(buttonName) {
-        case "daily": {
-            var buttonActionClass = document.getElementsByClassName("get-action-title");
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                for (let i = 0; i <= buttonActionClass.length; i++) {
-                    if (this.readyState == 4 && this.status == 200) {
-                        var myObj = JSON.parse(this.responseText);
-                        document.getElementById(buttonName).innerHTML = myObj.data.name;
-                    }
-                }
-            };
-            xmlhttp.open("GET", "data.json", true);
-            xmlhttp.send();
-            }   
-        }
-}
+var myId = "profile-daily-clicked";
+document.getElementById(myId).addEventListener("click", ttdmTfClicked(myId));
