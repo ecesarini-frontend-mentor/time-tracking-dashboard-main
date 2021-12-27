6b81265200cf2fc0c73db5433fa3b383d9d5971b
ecesarini <73652902+ecesarini@users.noreply.github.com>
2021-12-27 20:20:49 +0100

    edited js

--- ttdm.js
+++ ttdm.js
@@ -1,6 +1,23 @@
-function ttdmTfClicked(myId) {
-	var rtn = document.getElementById(myId).value;
-	alert(rtn + " has been clicked")
+function ttdmGetJson(myTarg, myId) {
+	myTarg.innerHtml = "You clicked " + myId;
+	/*var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var myObj = JSON.parse(this.responseText);
+                document.getElementById(prlId).innerHTML = myObj.data.name;
+            }
+    };
+    xmlhttp.open("GET", "data.json", true);
+    xmlhttp.send();*/
 }
-var myId = "profile-daily-clicked";
-document.getElementById(myId).addEventListener("click", ttdmTfClicked(myId));
+
+function ttdmTfClicked() {
+	var prl = document.getElementsByClassName("fb-profile-intervals")[0].getElementsByTagName("input");
+	for(let i = 0; i <= prl.length; i++) {
+		prl[i].addEventListener("click", function( prl[i], prl[i].id ) {
+
+		});
+	}
+}
+
+ttdmTfClicked();
