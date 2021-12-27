4da7a20256243a5221385175afdb50f2d53afc17
ecesarini <73652902+ecesarini@users.noreply.github.com>
2021-12-27 20:58:52 +0100

    Update ttdm.js

--- ttdm.js
+++ ttdm.js
@@ -1,5 +1,6 @@
 function ttdmGetJson(myTarg, myId) {
-	myTarg.innerHtml = "You clicked " + myId;
+	//myTarg.innerHtml = "You clicked " + myId;
+    console.log("You clicked" + myId);
 	/*var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
@@ -14,9 +15,7 @@ function ttdmGetJson(myTarg, myId) {
 function ttdmTfClicked() {
 	var prl = document.getElementsByClassName("fb-profile-intervals")[0].getElementsByTagName("input");
 	for(let i = 0; i <= prl.length; i++) {
-		prl[i].addEventListener("click", function( prl[i], prl[i].id ) {
-
-		});
+		prl[i].addEventListener("click", ttdmGetJson( prl[i], prl[i].id ));
 	}
 }
 
