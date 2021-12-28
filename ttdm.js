67e173b784f8c816b378aacc53a00a18b5cf71ee
ecesarini <73652902+ecesarini@users.noreply.github.com>
2021-12-28 23:24:30 +0100

    Update ttdm.js

--- ttdm.js
+++ ttdm.js
@@ -1,7 +1,7 @@
 function ttdmGetJson() {
     var thisId = this.id;
 	//myTarg.innerHtml = "You clicked " + myId;
-    alert("You clicked " + thisId);
+    //alert("You clicked " + thisId);
 	var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
