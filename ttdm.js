5bc5dc17a0cce8485c4411878fcc2ec78af27d3e
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-01-08 23:51:37 +0100

    Update ttdm.js

--- ttdm.js
+++ ttdm.js
@@ -12,7 +12,7 @@ function ttdmDailyAction() {
 
 function ttdmGlobalUpdateAction() {
     var actionType = this.id;
-    $.getJSON('data.json', function(data){
+    $.getJSON('data.json', function(data) {
     var actionClass = document.getElementsByClassName("fb-action-timeframes");
         for(let i = 0; i < actionClass.length; i++) {
             let actionFields = actionClass[i].getElementsByTagName("li");
@@ -37,12 +37,35 @@ function ttdmGlobalUpdateAction() {
 
 function ttdmGridItemUpdateAction() {
     // this.id e richiami classe
+    var actionTypeId = this.closest(".grid-element-action").id;
+    var timeFrame = this.innerText.toLowerCase();
+    var gridItemUpdate = document.getElementById(actionTypeId).getElementsByClassName("fb-action-timeframes")[0]
+        .getElementsByTagName("li");
+    var i = []
+    $.getJSON('data.json', function(data) {
+        switch (timeFrame) {
+            case "daily":
+                gridItemUpdate[1].innerText = data[i].timeframes.daily.current + "hrs";
+                gridItemUpdate[2].innerText = "Yesterday - " + data[i].timeframes.daily.previous + "hrs";
+                break;
+            case "weekly":
+                gridItemUpdate[1].innerText = data[i].timeframes.weekly.current + "hrs";
+                gridItemUpdate[2].innerText = "Last Week - " + data[i].timeframes.weekly.previous + "hrs";
+                break;
+            case "monthly":
+                gridItemUpdate[1].innerText = data[i].timeframes.monthly.current + "hrs";
+                gridItemUpdate[2].innerText = "Last Month - " + data[i].timeframes.monthly.previous + "hrs";
+                break;            
+        }
+    });
 }
 
 function ttdmTfClicked() {
-    //var isClicked = true;
 	document.getElementsByClassName("fb-profile-intervals")[0].querySelectorAll("input").forEach(item => item.addEventListener("click", ttdmGlobalUpdateAction, true));
-    document.getElementsByClassName("action-dropdown-content")[0].querySelectorAll("button").forEach(item => item.addEventListener("click", ttdmGridItemUpdateAction));
+    var actionDropdownClasses = document.getElementsByClassName("action-dropdown-content");
+    for(var i = 0; i < actionDropdownClasses.length; i ++) {
+        actionDropdownClasses[i].querySelectorAll("button").forEach(item => item.addEventListener("click", ttdmGridItemUpdateAction));
+    }
 }
 
 ttdmDailyAction();
