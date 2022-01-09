1c69a64eae36a4548be023942741e594d481d4a6
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-01-10 00:03:40 +0100

    Update ttdm.js
    gird-element  works;
    get into change background on hover mouse event;

--- ttdm.js
+++ ttdm.js
@@ -36,37 +36,44 @@ function ttdmGlobalUpdateAction() {
 }
 
 function ttdmGridItemUpdateAction() {
-    // this.id e richiami classe
     var actionTypeId = this.closest(".grid-element-action").id;
     var timeFrame = this.innerText.toLowerCase();
     var gridItemUpdate = document.getElementById(actionTypeId).getElementsByClassName("fb-action-timeframes")[0]
         .getElementsByTagName("li");
-    var i = []
     $.getJSON('data.json', function(data) {
+        let actionTypeIdJson = actionTypeId[0].toUpperCase() + actionTypeId.slice(1);
+        let dataCatch = data.filter((item) => {return item.title == actionTypeIdJson});
         switch (timeFrame) {
             case "daily":
-                gridItemUpdate[1].innerText = data[i].timeframes.daily.current + "hrs";
-                gridItemUpdate[2].innerText = "Yesterday - " + data[i].timeframes.daily.previous + "hrs";
+                gridItemUpdate[1].innerText = dataCatch[0].timeframes.daily.current + "hrs";
+                gridItemUpdate[2].innerText = "Yesterday - " + dataCatch[0].timeframes.daily.previous + "hrs";
                 break;
             case "weekly":
-                gridItemUpdate[1].innerText = data[i].timeframes.weekly.current + "hrs";
-                gridItemUpdate[2].innerText = "Last Week - " + data[i].timeframes.weekly.previous + "hrs";
+                gridItemUpdate[1].innerText = dataCatch[0].timeframes.weekly.current + "hrs";
+                gridItemUpdate[2].innerText = "Last Week - " + dataCatch[0].timeframes.weekly.previous + "hrs";
                 break;
             case "monthly":
-                gridItemUpdate[1].innerText = data[i].timeframes.monthly.current + "hrs";
-                gridItemUpdate[2].innerText = "Last Month - " + data[i].timeframes.monthly.previous + "hrs";
+                gridItemUpdate[1].innerText = dataCatch[0].timeframes.monthly.current + "hrs";
+                gridItemUpdate[2].innerText = "Last Month - " + dataCatch[0].timeframes.monthly.previous + "hrs";
                 break;            
         }
     });
 }
 
+function ttdmGridItemUpdateBg() {
+    let ddBgColor = this.closest(".fb-ico-bg").style.backgroundColor;
+    this.style.backgroundColor = ddBgColor;
+}
+
 function ttdmTfClicked() {
 	document.getElementsByClassName("fb-profile-intervals")[0].querySelectorAll("input").forEach(item => item.addEventListener("click", ttdmGlobalUpdateAction, true));
     var actionDropdownClasses = document.getElementsByClassName("action-dropdown-content");
-    for(var i = 0; i < actionDropdownClasses.length; i ++) {
+    for(let i = 0; i < actionDropdownClasses.length; i ++) {
+        actionDropdownClasses[i].parentElement.addEventListener("click", ttdmGridItemUpdateBg);
         actionDropdownClasses[i].querySelectorAll("button").forEach(item => item.addEventListener("click", ttdmGridItemUpdateAction));
     }
 }
+
 
 ttdmDailyAction();
 ttdmTfClicked();
